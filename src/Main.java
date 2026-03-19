46f591885f5224949f66f9f37b16296145f82e97
Arlan <2007arlan2007@gmail.com>
2026-03-19 10:50:31 +0500

    2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        task1(a);
+
+    }
+
+    public static void task1(int a) {
+        if (a < 10) System.out.println(a);
+        else {
+            task1(a / 10);
+            System.out.println(a % 10);
+        }
+    }
+}
